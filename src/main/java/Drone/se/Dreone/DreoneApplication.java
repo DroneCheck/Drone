3f3d68bc80700f29ed0f07a1ef3dd22d90ff7aef
manaf <manafsayade92@gmail.com>
2022-05-23 10:39:30 +0200

    Initial commit

---
+++ src/main/java/Drone/se/Dreone/DreoneApplication.java
@@ -0,0 +1,13 @@
+package Drone.se.Dreone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Drone/se/Dreone/DreoneApplicationTests.java
@@ -0,0 +1,13 @@
+package Drone.se.Dreone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
