bdac3f7c813fd20ab486da5a80fdd23b462118e5
samaarahmed <m.samaar@gmail.com>
2022-05-24 11:06:26 +0200

    Application properties added .Project up and running without errors

--- src/main/java/Drone/se/Dreone/DreoneApplication.java
+++ src/main/java/Drone/se/Dreone/DreoneApplication.java
@@ -8,7 +8,6 @@ public class DreoneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DreoneApplication.class, args);
-		System.out.println("Hello to Drone project");
 	}
 
 }
