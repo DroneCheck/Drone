b9af1c188a55224005b52d3f46df978ab952b7e1
DemirGv <gvargis.demir@gmail.com>
2022-05-25 11:36:05 +0200

    Initial commit

---
+++ Drone/src/test/java/Drone/se/Drone/DroneApplicationTests.java
@@ -0,0 +1,13 @@
+package Drone.se.Drone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
